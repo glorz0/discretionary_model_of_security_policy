ce5815b5a621724ab71b54b7f3c9c69d74216b9b
glorz0 <tudasuda1133@gmail.com>
2024-03-24 18:24:20 +0300

    добовление данных

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-Hello from Sri-Lanka
+users = ('Ivan', 'Boris', 'Roman', 'Emil', 'Adel', 'Sergey', 'Rovlan', 'Masha')
+objects = ('text_0.txt', 'text_1.txt','text_2.txt','text_3.txt','text_4.txt','text_5.txt',)
+acsesses = ('Чтение', 'Права на запись', 'Полный доспуп', 'права на исполнение', 'Права на передачу прав')
+def main():
+    user_input = input("Введите имя пользователя: ")
+    if user_input in users:
+        print("Привет," + user_input + '!')
+    else:
+        print("Пользователь не найден")
+
+
+
+
+
+
+if __name__ == '__main__':
+    main() 
+#matrix_of_acsess = { 
+#                   'Ivan':{
+#                           'text_0.txt':['Чтние']
+#                           }, 
+#                   }
+#
+#
