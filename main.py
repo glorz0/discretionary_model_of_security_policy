480fdfe941ba5c656d9a6b80895006f7b9e35582
glorz0 <tudasuda1133@gmail.com>
2024-03-24 19:14:04 +0300

    refactorin user identification

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ users = ('Ivan', 'Boris', 'Roman', 'Emil', 'Adel', 'Sergey', 'Rovlan', 'Masha')
 objects = ('text_0.txt', 'text_1.txt','text_2.txt','text_3.txt','text_4.txt','text_5.txt',)
 acsesses = ('Чтение', 'Права на запись', 'Полный доспуп', 'права на исполнение', 'Права на передачу прав')
 def main():
-    user_input = input("Введите имя пользователя: ")
-    if user_input in users:
-        print("Привет," + user_input + '!')
-    else:
-        print("Пользователь не найден")
+        while True:
+            user_input = input("Введите имя пользователя: ")
+            if user_input in users:
+                print("Привет," + user_input + '!')
+                break
+            else:
+                print("Пользователь не найден")
 
 
 
